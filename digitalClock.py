070019d76060b43eb8d9fc5b24fcccf6d3abf634
Tausif Raza <tausifr13@gmail.com>
2023-07-11 15:20:49 +0530

    Update digitalClock.py

--- digitalClock.py
+++ digitalClock.py
@@ -1,7 +1,19 @@
 from tkinter import Tk, Label
+from datetime import datetime
 
 window = Tk()
 window.title("Digital Clock")
 window.geometry("600x300")
+window.configure(bg="steelblue")
+
+Label = Label(window, font=("Arial White",78,"bold"), bg="steelblue")
+Label.pack(pady=100)
+
+def clock():
+    time = datetime.now().strftime("%H:%M:%S")
+    Label.configure(text=time)
+    Label.after(500,clock)
+
+clock()
 
 window.mainloop()
